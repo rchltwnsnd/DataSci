87f726e9859cef7c6721bea8b64a6a8e02b40bd9
Rachel Townsend <rchl.twnsnd@gmail.com>
2014-07-10 23:32:21 -0700

    some progress
    still breaking on unicode strings

--- 1-2.py
+++ 1-2.py
@@ -1,5 +1,5 @@
 import json
-
+import pdb
 afinnfile = open("AFINN-111.txt")
 scores = {} # initialize an empty dictionary
 for line in afinnfile:
@@ -13,12 +13,31 @@ for tweet in stream_data:
     line = tweet.splitlines()
     tweets.extend(line)
 
-i = 0
-wtf = []
-while i < 5:
-    j = json.loads(tweets)
-    if 'text' in j.keys():
-      wtf.extend(j['text'])
-    i +=1
+# each tweets is a dictionary
 
-print wtf
+def clean_word(word):
+	return word.lower().strip('.,"[]\'\n?!-_()$%~*+&/@')
+
+# words_list is a list of lists of the words in each tweet 
+words_list = []
+for i in range(len(tweets)):
+	j = json.loads(tweets[i])
+	if 'text' in j.keys():
+		words_list.append(j['text'].split())
+
+i = 0
+t_score = 0
+for i in range(len(words_list)):
+	for w in words_list[i]:
+		if clean_word(w) in scores.keys():
+			t_score = t_score + scores[w]
+i +=1
+# for w in words:
+# 	if clean_word(w) in scores.keys():
+# 		print w + scores[clean_word(w)]
+# else:
+# 	print 'nope'
+
+print t_score
+
+#unicode warning: unicode equal comparison failed to convert both argments to unicode
